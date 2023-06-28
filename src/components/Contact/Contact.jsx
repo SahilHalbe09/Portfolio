fc4df97d9fa29739cfa5b5855bc36917e81dd004
Sahil Halbe <sahilyhalbe@gmail.com>
2023-06-28 14:56:18 +0530

    updated resume

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,18 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import "./contact.css";
 
 const Contact = () => {
 	const form = useRef();
 
+	const [loading, setLoading] = useState(false);
+
 	// console.log(process.env.REACT_APP_YOUR_TEMPLATE_ID, process.env.REACT_APP_YOUR_SERVICE_ID, process.env.REACT_APP_YOUR_PUBLIC_KEY);
 
 	const sendEmail = (e) => {
 		e.preventDefault();
+
+		setLoading(true);
 
 		emailjs.sendForm("service_1qld9rq", "template_uvd2anr", form.current, "wRXVxGTYpTv8fmpJN").then(
 			(result) => {
@@ -18,6 +22,10 @@ const Contact = () => {
 				console.log(error.text);
 			}
 		);
+
+		setTimeout(() => {
+			setLoading(false);
+		}, 1000);
 	};
 
 	return (
@@ -32,19 +40,19 @@ const Contact = () => {
 				<form className="contact__form" ref={form} onSubmit={sendEmail}>
 					<div className="contact__form-group">
 						<div className="contact__form-div">
-							<input type="text" name="user_name" className="contact__form-input" placeholder="Your name" />
+							<input type="text" name="user_name" className="contact__form-input" placeholder="Your name" required />
 						</div>
 						<div className="contact__form-div">
-							<input type="email" name="user_email" className="contact__form-input" placeholder="Your email" />
+							<input type="email" name="user_email" className="contact__form-input" placeholder="Your email" required />
 						</div>
 					</div>
 					<div className="contact__form-div">
-						<input type="text" name="subject" className="contact__form-input" placeholder="Subject line" />
+						<input type="text" name="subject" className="contact__form-input" placeholder="Subject line" required />
 					</div>
 					<div className="contact__form-div contact__form-area">
-						<textarea name="message" cols="30" rows="10" className="contact__form-input" placeholder="Your Message"></textarea>
+						<textarea name="message" cols="30" rows="10" className="contact__form-input" placeholder="Your Message" required></textarea>
 					</div>
-					<input type="submit" value="Send" className="btn" />
+					<input type="submit" value={loading ? "Sending..." : "Send"} className="btn" />
 				</form>
 			</div>
 		</section>
